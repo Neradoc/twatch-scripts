827f93d7b4ce768c9a4d1f803e67120b3486be30
Neradoc <neraOnGit@ri1.fr>
2023-03-18 01:28:05 +0100

    use constants not strings in IRQ

--- code_test_button.py
+++ code_test_button.py
@@ -4,7 +4,7 @@ from digitalio import *
 import board
 import time
 
-from axp202 import AXP202
+from axp202 import AXP202, IRQ
 axp202 = AXP202(board.I2C())
 axp202.set_ldo2(True)
 
@@ -13,9 +13,9 @@ irq_axp = DigitalInOut(board.AXP202_INT)
 while True:
     if not irq_axp.value:
         irqs = axp202.get_irqs()
-        if "SHORT_PRESS" in irqs:
+        if IRQ.SHORT_PRESS in irqs:
             print("Short press")
-        if "LONG_PRESS" in irqs:
+        if IRQ.LONG_PRESS in irqs:
             print("Long press")
     time.sleep(0.1)
 
